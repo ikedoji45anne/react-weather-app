08cc84b4ae5783d0939c61dfcfb80f3aca64ec0d
ikedoji45anne <148642109+ikedoji45anne@users.noreply.github.com>
2024-06-24 23:19:58 +0100

    added link to github.com

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     Weather App
+     <h1> Weather App </h1>
+     <footer>
+      This project was coded by Ikedoji Anne and is[" "]
+      <a href="https://github.com/ikedoji45anne/react-weather-app" target="_blank">
+      open-sourced on github
+      </a>
+     </footer>
     </div>
   );
 }
