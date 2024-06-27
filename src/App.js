c7c0ad5c749f4de58bb0ad2fd75149c2f6ddee74
ikedoji45anne <148642109+ikedoji45anne@users.noreply.github.com>
2024-06-27 21:08:18 +0100

    created the weather App and added the html components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
-function App() {
+    export default function App() {
   return (
     <div className="App">
       <div className="container">
-     <h1> Weather App </h1>
+     
+     <Weather />
      <footer>
-      This project was coded by Ikedoji Anne and is 
+      
+      This project was coded by{" "} Ikedoji Anne and is{" "} 
       <a href="https://github.com/ikedoji45anne/react-weather-app" target="_blank">
       open-sourced on github
       </a>
      </footer>
      </div>
+     
 
     </div>
   );
+  
 }
 
-export default App;
+ 
