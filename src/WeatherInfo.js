2b26333f27c68342ea8f314fa5126c18ef6c686d
ikedoji45anne <148642109+ikedoji45anne@users.noreply.github.com>
2024-07-08 03:01:19 +0100

    created a unit component and tracked the unit value

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 
 
@@ -23,13 +24,9 @@ export default function WeatherInfo(props) {
                         className="float-left"
                         
                         />
-                          
-
-                        
-                        
-                        <div className="float-left">
-                        <span className="temperature">{Math.round(props.data.temperature)}</span>
-                        <span className="unit">°C</span>
+                     <div className="float-left">
+                        <WeatherTemperature  celsius={props.data.temperature}/>
+                       
                         </div>
                         </div>
                         </div>
